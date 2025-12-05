94010edb110d14c4ba149fd58b98a3746cf5639c
Rob Watson <source.pot.dev@gmail.com>
2025-12-05 09:15:02 +0000

    Simplify day 5 solution

--- day05/day05-part2.php
+++ day05/day05-part2.php
@@ -1,9 +1,11 @@
 <?php
 
 $input = file_get_contents(__DIR__ . '/real-input');
-[$ranges, $ingredients] = explode("\n\n", $input, 2);
-$ranges = explode("\n", trim($ranges));
-$ranges = array_map(fn ($line) => explode('-', trim($line)), $ranges);
+$ranges = array_map(
+    callback: fn ($line) => explode('-', trim($line)),
+    array: explode("\n", explode("\n\n", $input, 2)[0])
+);
+sort($ranges);
 
 for ($r = 0; $r < count($ranges)-1; $r++) {
     if ($ranges[$r] === null) continue;
@@ -15,36 +17,20 @@ for ($r = 0; $r < count($ranges)-1; $r++) {
 
         [$rrStart, $rrEnd] = $ranges[$rr];
 
-        // first range is completely inside second range - delete first range
-        if ($rStart >= $rrStart && $rEnd <= $rrEnd) {
-            $ranges[$r] = null;
+        if ($rrStart > $rEnd) {
+            // no overlap, these 2 ranges can coexist
             continue;
         }
 
-        // second range is completely inside first range - delete second range
-        if ($rrStart >= $rStart && $rrEnd <= $rEnd) {
-            $ranges[$rr] = null;
-            continue;
-        }
-
-        // first range starts before second range - move second range start, delete first range
-        if ($rStart <= $rrStart && $rEnd >= $rrStart) {
-            $ranges[$rr][0] = $rStart;
-            $ranges[$r] = null;
-            continue;
-        }
-
-        // first range ends after second range - move second range end, delete first range
-        if ($rEnd >= $rrEnd && $rStart <= $rrEnd) {
-            $ranges[$rr][1] = $rEnd;
-            $ranges[$r] = null;
-            continue;
-        }
-
-        // ranges don't overlap - ignore
+        // set end to max of both ends
+        $ranges[$r][1] = $rEnd = max($rEnd, $rrEnd);
+        $ranges[$rr] = null;
     }
 }
 
+/**
+ * Example: 10 -> 20 has 11 numbers.  20-10 === 10, so you have to +1 after
+ */
 $ans = array_reduce(
     array: array_filter($ranges),
     callback: fn ($carry, $range) => $carry + $range[1] - $range[0] + 1,
