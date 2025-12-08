ea66278f4387f75698eb9df99ee4a7fc22174145
Rob Watson <source.pot.dev@gmail.com>
2025-12-08 20:57:41 +0000

    Day 8, part 1 finished

--- day08/day08-part1.php
+++ day08/day08-part1.php
@@ -1,20 +1,17 @@
 <?php
 
-# Make 10 shortest connections with example data
-# Make 1000 shortest connections with real data
-
 $input = array_map(
     trim(...),
-    file(__DIR__ . '/example-input'),
+    file(__DIR__ . '/real-input'),
 );
 
-function dist(int $x, int $y, int $z): float
+function dist(int $x, int $y, int $z): int
 {
     return $x * $x + $y * $y + $z * $z;
 }
 
 $distances = [];
-for ($i = 0; $i < count($input); $i++) {
+for ($i = 0; $i < count($input)-1; $i++) {
     $a = $input[$i];
     $ac = array_map(intval(...), explode(',', $a));
 
@@ -32,45 +29,66 @@ for ($i = 0; $i < count($input); $i++) {
 
 asort($distances);
 
+echo "Calculated ".count($distances)." distances\n";
+
+# todo change to 1000 for real input
+array_splice($distances, 1000);
+
 // build circuits using the closest boxes that aren't already in a circuit
 $circuits = [];
-# todo change to 1000 for real input
 $connections = 0;
 
 foreach ($distances as $next => $_) {
-    if ($connections++ === 11) {
-        break;
-    }
-
     [$a, $b] = explode('-', $next);
-//    echo "Checking $a >> $b\n";
 
     foreach ($circuits as $circuit) {
         if (in_array($a, $circuit) && in_array($b, $circuit)) {
             // we have already added both of these to the same circuit
-//            echo "In same circuit: $a >> $b\n";
             continue 2;
         }
     }
 
     // if not, attach to a circuit
+    $found = false;
     for ($c = 0; $c < count($circuits); $c++) {
         $circuit = $circuits[$c];
         if (in_array($a, $circuit)) {
             $circuits[$c][] = $b;
-//            echo "A already in circuit, adding B\n";
-            continue 2;
+            $found = true;
+            break;
         }
         if (in_array($b, $circuit)) {
             $circuits[$c][] = $a;
-//            echo "B already in circuit, adding A\n";
-            continue 2;
+            $found = true;
+            break;
         }
     }
 
-    // not added to any circuit?  Add them both to a new circuit
-//    echo "Neither already in a circuit\n";
-    $circuits[] = [$a,$b];
+    if (!$found) {
+        // not added to any circuit?  Add them both to a new circuit
+        $circuits[] = [$a, $b];
+    }
+
+    // dedupe circuits list
+    for ($c = 0; $c < count($circuits)-1; $c++) {
+        for ($cc = $c+1; $cc < count($circuits); $cc++) {
+            if (count(array_intersect($circuits[$c], $circuits[$cc])) > 0) {
+                // a junction appears in both $circuits[$c] AND $circuits[$cc] so we should merge them
+                $circuits[$c] = array_unique(array_merge($circuits[$c], $circuits[$cc]));
+                array_splice($circuits, $cc, 1);
+                break 2;
+            }
+        }
+    }
 }
 
-var_dump($circuits);
+$sizes = array_map(fn (array $circuit) => count($circuit), $circuits);
+rsort($sizes);
+$top3 = array_slice($sizes, 0, 3);
+$answer = array_reduce(
+    $top3,
+    fn (int $carry, int $num) => $carry * $num,
+    1,
+);
+
+echo "Answer: $answer\n";
